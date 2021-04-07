9777c4f60bf6f905d256b6946ef3d946aa9b2ed5
RáczBianka <bianka.racz@gmail.com>
2021-04-07 18:20:20 +0200

    ok

---
+++ Gyakorlas0407/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas0407
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"fájlbeolvasás, tárolás");
+            Console.WriteLine($"adatok száma");
+            Console.WriteLine($"legnagyobb elem helye");
+            Console.WriteLine($"van budapesti lakos");
+            Console.WriteLine($"mindenki 18 feletti");
+            Console.WriteLine($"ki lakik a XIII. kerületben");
+            Console.WriteLine($"milyen kerületekben laknak");
+            Console.WriteLine($"melyik kerületben hányan laknak");
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
