8288573bdd4ce6d947351ce5080f5af834598d24
RáczBianka <bianka.racz@gmail.com>
2021-04-07 18:54:57 +0200

    txt_1

---
+++ Gyakorlas0407/Adat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas0407
+{
+    class Adat
+    {
+        //fejléc: Név;Kor;Hely;Ker
+        private string nev;
+        private int kor;
+        private string hely;
+        private int ker;
+
+
+        public Adat(string fajlEgySora)
+        {
+
+            string[] s = fajlEgySora.Split('|');
+            //indexek:     0     1  2   3
+            //fájl 1 sora: Petra;19;Bp;20
+            this.nev = s[0];
+            this.kor = int.Parse(s[1]);
+            this.hely = s[2];
+            this.ker = int.Parse(s[2]);
+
+        }
+
+
+    }
+}
--- Gyakorlas0407/Program.cs
+++ Gyakorlas0407/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,16 @@ namespace Gyakorlas0407
             Console.WriteLine($"ki lakik a XIII. kerületben");
             Console.WriteLine($"milyen kerületekben laknak");
             Console.WriteLine($"melyik kerületben hányan laknak");
-            
-            
-            
+
+            string[] sorok = File.ReadAllLines("adat3.txt");
+            List<Adat> adatok = new List<Adat>();
+            foreach (string sor in sorok.Skip(1))
+            {
+                adatok.Add(new Adat(sor));
+            }
+
+          
+
             Console.ReadLine();
         }
     }
